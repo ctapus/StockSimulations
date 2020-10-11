ed9f66e5e855b61f5656942d309ab3269d19eef9
ctapus <ctapus@hotmail.com>
2020-10-11 23:02:17 +0200

    Multiple strategies added.

--- src/index.ts
+++ src/index.ts
@@ -32,19 +32,65 @@ class HistoryItem {
     }
 }
 interface ConditionSelector {
-    (tradeDate: TradeData): boolean;
+    (tradeData: TradeData): boolean;
 }
 const conditionSelectorsMap: { [id: string]: ConditionSelector } = {
-    "DAILY":                (tradeDate: TradeData): boolean => { return true; },
-    "CUR_LOWER_PREV":       (tradeDate: TradeData): boolean => { return tradeDate.open < tradeDate.previousDay?.open; },
-    "CUR_3%LOWER_PREV":     (tradeDate: TradeData): boolean => { return 0.97 * tradeDate.open < tradeDate.previousDay?.open },
-    "CUR_HIGHER_PREV":      (tradeDate: TradeData): boolean => { return tradeDate.open > tradeDate.previousDay?.open; },
-    "CUR_3%HIGHER_PREV":    (tradeDate: TradeData): boolean => { return 0.97 * tradeDate.open > tradeDate.previousDay?.open }
+    "DAILY":                (tradeData: TradeData): boolean => { return true; },
+    "CUR_LOWER_PREV":       (tradeData: TradeData): boolean => { return tradeData.open < tradeData.previousDay?.open; },
+    "CUR_3%LOWER_PREV":     (tradeData: TradeData): boolean => { return 0.97 * tradeData.open < tradeData.previousDay?.open },
+    "CUR_HIGHER_PREV":      (tradeData: TradeData): boolean => { return tradeData.open > tradeData.previousDay?.open; },
+    "CUR_3%HIGHER_PREV":    (tradeData: TradeData): boolean => { return 0.97 * tradeData.open > tradeData.previousDay?.open }
 }
 interface TimeSelector {
     (tradeDate: TradeData, startDate: Date): boolean;
 }
 const startingDateSelector : TimeSelector = (tradeDate: TradeData, startDate: Date): boolean => { return tradeDate.date >= startDate; };
+
+class Portofolio {
+    public amountOfMoney: number;
+    public numberOfShares: number;
+    public history: Array<HistoryItem>;
+    constructor(amountOfMoney: number, numberOfShares: number) {
+        this.amountOfMoney = amountOfMoney;
+        this.numberOfShares = numberOfShares;
+        this.history = new Array<HistoryItem>(); 
+    }
+}
+interface MappableAction {
+    (tradeData: TradeData, portofolio: Portofolio, numberOfShares: number): boolean;
+}
+const actionsMap: { [id: string]: MappableAction } = {
+    "BUY":                  (tradeData: TradeData, portofolio: Portofolio, numberOfShares: number): boolean => {
+                                let actionPrice: number = tradeData.open * numberOfShares;
+                                if(portofolio.amountOfMoney < actionPrice) {
+                                    return false;
+                                }
+                                portofolio.numberOfShares += numberOfShares;
+                                portofolio.amountOfMoney -= actionPrice;
+                                portofolio.history.push(new HistoryItem("BUY", tradeData.date, numberOfShares, tradeData.open));
+                            },
+    "SELL":                 (tradeData: TradeData, portofolio: Portofolio, numberOfShares: number): boolean => {
+                                if(portofolio.numberOfShares < numberOfShares) {
+                                    return false;
+                                }
+                                let actionPrice: number = tradeData.open * numberOfShares;
+                                portofolio.numberOfShares -= numberOfShares;
+                                portofolio.amountOfMoney += actionPrice;
+                                portofolio.history.push(new HistoryItem("SELL", tradeData.date, numberOfShares, tradeData.open));
+                            }
+}
+interface Action {
+    (tradeData: TradeData, portofolio: Portofolio): boolean;
+}
+
+class Strategy {
+    public conditionSelector: ConditionSelector;
+    public action: Action;
+    constructor(conditionSelector: ConditionSelector, action: Action) {
+        this.conditionSelector = conditionSelector;
+        this.action = action;
+    }
+}
 function getTimeValues(data: any): Array<TradeData> {
     let ret: Array<TradeData> = new Array<TradeData>();
     let previousDayTrade: TradeData = null;
@@ -64,9 +110,7 @@ function getTimeValues(data: any): Array<TradeData> {
 }
 $(() => {
     let timeValues: Array<TradeData>;
-    let history: Array<HistoryItem>;
-    let lastOpen: number = 0;
-    let startDate: Date;
+    const strategies: Array<Strategy> = new Array<Strategy>();
     $("#ticker").on("change", function() {
         let ticker: string = $("#ticker").val().toString();
         $("#startDate").prop("disabled", true);
@@ -78,9 +122,7 @@ $(() => {
         $.getJSON(`.\\alphavantage\\${ticker}.json`, (data) => {
             timeValues = getTimeValues(data);
             timeValues.sort((a, b) => a.date.getTime() - b.date.getTime());
-            startDate = timeValues[0].date;
-            lastOpen = timeValues[timeValues.length - 1].open;
-            $("#startDate").val(startDate.toISOString().split('T')[0]);
+            $("#startDate").val(timeValues[0].date.toISOString().split('T')[0]);
             timeValues.forEach(item => {
                 $('#data > tbody').append("<tr><td>" + item.date.toISOString().split('T')[0] + "</td><td>" + item.open + "</td><td>" + item.high +
                     "</td><td>" + item.low +"</td><td>" + item.close +"</td><td>" + item.volume +"</td></tr>");
@@ -96,43 +138,31 @@ $(() => {
         });
     });
     $("#addStrategy").on("click", function() {
-        let startingAmount: number = Number($("#startingAmount").val());
+        $("#run").prop("disabled", false);
         let actionText: string = $("#action option:selected").text();
         let action: string = $("#action option:selected").val().toString();
         let numberOfShares: number = Number($("#numberOfShares").val());
         let conditionText: string = $("#condition option:selected").text();
         let condition: string = $("#condition option:selected").val().toString();
+        strategies.push(new Strategy(conditionSelectorsMap[condition], (tradeData: TradeData, portofolio: Portofolio): boolean => { return actionsMap[action](tradeData, portofolio, numberOfShares) }));
+        $("#globalStrategy").append(`<p>${actionText} ${numberOfShares} shares ${conditionText}</p>`);
+    });
+    $("#run").on("click", function() {
+        let startingAmount: number = Number($("#startingAmount").val());
         let startDate: Date = new Date($("#startDate").val().toString());
         
-        let endingNumberOfShares: number = 0;
-        let endingMoney: number = startingAmount;
-        history = new Array<HistoryItem>();
+        let portofolio: Portofolio = new Portofolio(startingAmount, 0);
         timeValues.filter((item) => { return startingDateSelector(item, startDate); }).forEach(item => {
-            switch(action){
-                case "BUY" :
-                    {
-                        let actionPrice: number = item.open * numberOfShares;
-                        if(endingMoney < actionPrice) {
-                            return false;
-                        }
-                        if(conditionSelectorsMap[condition](item)) {
-                            endingNumberOfShares += numberOfShares;
-                            endingMoney -= actionPrice;
-                            history.push(new HistoryItem("BUY", item.date, numberOfShares, item.open));
-                        } else {
-                            return false;
-                        }
-                    }
-                case "SELL":
-                    break;
-            }
+            strategies.forEach((strategy: Strategy) => {
+                if(strategy.conditionSelector(item)) {
+                    strategy.action(item, portofolio);
+                } else {
+                    return false;
+                }
+            });
         });
-        $("#strategies").append(`<p>${actionText} ${numberOfShares} shares ${conditionText}</p>`);
-        $("#strategies").append(`<p>${endingNumberOfShares} shares x ${lastOpen.toFixed(2)} $ = ${(endingNumberOfShares * lastOpen).toFixed(2)} $. And ${endingMoney.toFixed(2)} $ left. Total = ${(endingNumberOfShares * lastOpen + endingMoney).toFixed(2)}</p>`);
-        $("#strategies").append("<br />");
-        $("#strategies").append("<br />");
-        history.forEach((item: HistoryItem) => {
-            $("#strategies").append(`On ${item.date.toLocaleDateString()} ${item.action} ${item.numberOfShares} shares for ${item.sharePrice} each.<br />`);
+        portofolio.history.forEach((item: HistoryItem) => {
+            $("#results").append(`On ${item.date.toLocaleDateString()} ${item.action} ${item.numberOfShares} shares for ${item.sharePrice} each.<br />`);
         });
     });
 });
