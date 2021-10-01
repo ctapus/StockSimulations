4c82e97c96753ac9c1b79f8f0a0b8b681f0a7848
ctapus <ctapus@hotmail.com>
2021-10-01 22:25:02 +0200

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.ts',
     indexIntraday: './src/indexIntraday.ts',
@@ -9,8 +10,9 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, './scripts'),
-    filename: './[name].js'
+    path: path.resolve(__dirname, './public/scripts'),
+    filename: './[name].js',
+    clean: true
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
@@ -25,19 +27,19 @@ module.exports = {
     ]
   },
   plugins: [
-      new webpack.ProvidePlugin({            
+      new webpack.ProvidePlugin({
           $: 'jquery',
           jQuery: 'jquery',
           jquery: 'jquery'
       })
   ],
   devServer: {
-      stats: 'errors-only',
+      compress: true,
       host: process.env.HOST,
       port: process.env.PORT,
-      open: true,
-      overlay: true,
-      contentBase: './',
-      openPage: ''
+      open: ['/index.html'],
+      client: {
+        overlay: true,
+      },
     }
 };
