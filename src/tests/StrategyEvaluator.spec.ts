a43e570770141a20bf896ae5841d7973471d738d
ctapus <ctapus@hotmail.com>
2022-04-02 16:56:49 +0300

    Updated parse test

--- src/tests/StrategyEvaluator.spec.ts
+++ src/tests/StrategyEvaluator.spec.ts
@@ -24,9 +24,14 @@ describe("BooleanEvaluatorMath test suite", () => {
 		token = lexer.getTokenAndAdvance();
 		expect(token.type).to.be.equal(StrategyTokenType.End);
 	});
-	it("Can parse", () => {
+	it("Can parse one branch", () => {
 		const parser: StrategyParser = new StrategyParser();
         let strategy: Strategy = parser.parse("BUY_PERCENTAGE 100 WHEN PREV_DAY_OPEN < 0.97 * DAY_OPEN;");
+		expect(strategy).to.be.not.null;
+	});
+	it("Can parse two branches", () => {
+		const parser: StrategyParser = new StrategyParser();
+        let strategy: Strategy = parser.parse("SELL_PERCENTAGE 100 WHEN 1 * DAY_OPEN >= 1.03 * PREV_DAY_OPEN; BUY_PERCENTAGE 100 WHEN 1 * DAY_OPEN <= 0.97 * PREV_DAY_OPEN; ");
 		expect(strategy).to.be.not.null;
 	});
 });
